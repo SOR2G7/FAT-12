9e5cc3401126200d106751f1bc807f0f2db52985
ArriolaMatias <arriolamattias@gmail.com>
2024-04-18 12:55:41 -0300

    Ajustado print para ser mas descriptivo con la consigna

--- read_first_partition.c
+++ read_first_partition.c
@@ -8,14 +8,14 @@ int main() {
     fseek(in, 446 , SEEK_SET); // Voy al inicio.
     
     for(i=0; i<1; i++) { // Leo las entradas
-        printf("Partition entry %d: First byte %02X\n", i, fgetc(in));
+        printf("Partition entry %d:\n  Bootable flag: %02X\n", i, fgetc(in));
         printf("  Comienzo de partición en CHS: %02X:%02X:%02X\n", fgetc(in), fgetc(in), fgetc(in));
         printf("  Partition type 0x%02X\n", fgetc(in));
         printf("  Fin de partición en CHS: %02X:%02X:%02X\n", fgetc(in), fgetc(in), fgetc(in));
         
         fread(&start_sector, 4, 1, in);
         fread(&length_sectors, 4, 1, in);
-        printf("  Dirección LBA relativa 0x%08X, de tamaño en sectores %d\n", start_sector, length_sectors);
+        printf("  Dirección LBA relativa: 0x%08X\n  Tamaño en sectores: %d\n", start_sector, length_sectors);
     }
     
     fclose(in);
